a4506921961d23c1c652a8346a6fd7b927601b1f
william-wv <wwillveiga@hotmail.com>
2025-11-09 08:13:19 -0300

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Sidebar } from "./components/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterModule],
+  imports: [RouterModule, Header, Sidebar],
 })
 export class App {
   title = signal('UTF-Check');
