41acf0f50899f466a6b21afc5c764791cf486846
William <wwillveiga@hotmail.com>
2025-11-27 23:02:27 -0300

    fix errors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp as angularInitializeApp } from '@angular/fire/app';
@@ -6,6 +7,15 @@ import { provideAuth, getAuth as angularGetAuth } from '@angular/fire/auth';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC_-i8sgUc9OcgZqdrqak52GkwGOU_P_k4",
   authDomain: "utfcheckapi.firebaseapp.com",
@@ -13,9 +23,14 @@ const firebaseConfig = {
   projectId: "utfcheckapi",
   storageBucket: "utfcheckapi.firebasestorage.app",
   messagingSenderId: "764300684295",
-  appId: "1:764300684295:web:9f567fe670d69ebb22cea2",
-  measurementId: "G-5ZLCJT39LC"
+  appId: "1:764300684295:web:5499379d6ce955a722cea2",
+  measurementId: "G-BE13YFGE07"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 bootstrapApplication(App, {
   providers: [
